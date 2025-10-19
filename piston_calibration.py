73a279bbcd4b473f20f3d95f881e9c05afd21ec0
emzet <emzet.mz507@gmail.com>
2025-10-19 14:03:57 +0200

    zmiana

--- piston_calibration.py
+++ piston_calibration.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-m = 1 #float(input("masa kg: "))
+m = float(input("masa kg: "))
 t = float(input("temperatura st C: "))
 rod = 998.56  #float(input("gęstość wody stanowiskowej w 20 st kg/m3: "))
 deltat = float(input("dopuszczalna zmiana temperatury podczas pomiaru st C:   "))
@@ -33,11 +33,11 @@ print (Vcmax , Vcmin)
 beta1 = (Vcmax - Vc) / deltat
 beta2 = (Vc - Vcmin) /deltat
 
-print (beta1, beta2)
+#print (beta1, beta2)
 
 avgbeta = round((beta1 + beta2)/2, 5)
 
-print (avgbeta)
+#print (avgbeta)
 
 
 Vmag = float(input("średnia objętość wody w cylindrze Vmag l:     "))
@@ -50,33 +50,42 @@ print ( uVmag)
 
 uVmagrel = round (uVmag / Vc *100, 2)
 
-print ("Względna niepewność poprawki związanej z magazynowaniem masy uVmagrel =" , uVmagrel ,"%")
+#print ("Względna niepewność poprawki związanej z magazynowaniem masy uVmagrel =" , uVmagrel ,"%")
 
 um = float(input("standardowa niepewność ważenia kg: "))
 
 ddm = ( objetosc(m + um, t, rod) - objetosc(m - um, t, rod)) / ( 2 * um)
-print ("Cm = " , ddm , "kg/l")
+#print ("Cm = " , ddm , "kg/l")
 
 ut = float(input("standardowa niepewność pomiaru temperatury st C: "))
 
 ddt = ( objetosc(m, t + ut, rod) - objetosc(m, t - ut, rod)) / ( 2 * ut)
-print ("Ct = " , ddt , "stC/l")
+#print ("Ct = " , ddt , "stC/l")
 
 
 urod = float(input("standardowa niepewność wyznaczenia gęstości wody stan. kg/m3: "))
 ddrod = ( objetosc(m, t, rod + urod) - objetosc(m, t, rod - urod)) / ( 2 * urod)
-print ("Crod = " , ddrod , "kg/m3/l")
+#print ("Crod = " , ddrod , "kg/m3/l")
 
 #udziały
 
-print ( "Udziały wartości źródłowych w niepewności" )
+#print ( "Udziały wartości źródłowych w niepewności" )
 
 masa = um * ddm
 temp = ut * ddt
 gestosc = urod * ddrod
 
-print ( " Udział niepewności ważenia:" , masa , " l " )
-print ( " Udział niepewności pomiaru temperatury wody:" , temp , " l " )
-print ( " Udział niepewności wyznaczenia gęstości wody:" , gestosc , " l " )
+#print ( " Udział niepewności ważenia:" , masa , " l " )
+#print ( " Udział niepewności pomiaru temperatury wody:" , temp , " l " )
+#print ( " Udział niepewności wyznaczenia gęstości wody:" , gestosc , " l " )
 
+#niepewność standardowa
 
+uvc = np.sqrt((um * masa)**2 + (ut * temp)**2 + (urod * gestosc)**2 + uVmag**2)
+
+print ("Standardowa niepewność Vc:" , uvc , " l ")
+
+uvcrel = round (uvc / Vc *100 , 2)
+
+print ("Względna niepewność Vc:" , uvcrel , "%")
+       
